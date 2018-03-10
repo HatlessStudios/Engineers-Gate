1ec550ea087f0833b031c200f2107701fe3c35c3
ScottRWood <productionspulsar@gmail.com>
2018-03-10 16:10:44 +0000

    Edit csv_reader to sort data by coin name

--- csv_reader.py
+++ csv_reader.py
@@ -1,15 +1,11 @@
 import csv
 
 with open("data/coin_data.csv", newline='') as csvfile:
-    coin_dict = {}
+    data = []
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        coin = row[0].split(",")[1]
-        if coin not in coin_dict.keys():
-            coin_dict[coin] = 1
-        else:
-            coin_dict[coin] += 1
+        data.append(row[0].split(","))
 
-    print(coin_dict)
-    coin_dict_under200 = {k: v for k, v in coin_dict.items() if v < 200}
-    print(len(coin_dict_under200))
+    data.sort(key=lambda x: x[1])
+
+    print(data)
