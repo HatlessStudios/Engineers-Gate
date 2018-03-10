634d6539d1a215db02f0c2dac7c8f41993b1cffe
= <chris.phippen98@gmail.com>
2018-03-10 20:36:23 +0000

    Create function to evaluate potential earnings

--- evaluate_earnings.py
+++ evaluate_earnings.py
@@ -1,6 +1,11 @@
-import model
+from model import get_score
 
-def get_input(prompt):
 
-    while True:
-        print(prompt)
+def evaluate_currency(cur_name, data):
+
+    if type(cur_name) == list:
+        for currency in cur_name:
+            print(currency, get_score(currency, data))
+
+    else:
+        print(cur_name, get_score(cur_name, data))
