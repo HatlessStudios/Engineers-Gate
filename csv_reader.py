bc579e4e4c60eb58bf67cde2bb11d0764cbb1840
ScottRWood <productionspulsar@gmail.com>
2018-03-10 16:00:06 +0000

    Finish csv_reader

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,15 @@
 import csv
 
 with open("data/coin_data.csv", newline='') as csvfile:
+    coin_dict = {}
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        print(' '.join(row))
+        coin = row[0].split(",")[1]
+        if coin not in coin_dict.keys():
+            coin_dict[coin] = 1
+        else:
+            coin_dict[coin] += 1
+
+    print(coin_dict)
+    coin_dict_under200 = {k: v for k, v in coin_dict.items() if v < 200}
+    print(len(coin_dict_under200))
