f24c5a9869d858e0d0969f9bf81e951083f9abe1
Chris Phippen <chris.phippen98@gmail.com>
2018-03-11 09:05:55 +0000

    It works!

--- main.py
+++ main.py
@@ -50,8 +50,10 @@ def show_graphs():
 
 def model_currency(k=10):
 
-    data = cr.read_csv()[1]
+    coin_dict, data = cr.read_csv()
+    data = cr.split_data_coins(coin_dict, data)[list(coin_dict.keys())[0]]
     model_weights = []
+    model_errors = []
 
     split_data = cr.split_data(data, k)
     split_data = [[[float(e[2]), float(e[5]), float(e[3]), float(e[4])] for e in s] for s in split_data]
@@ -65,7 +67,22 @@ def model_currency(k=10):
         training_data = np.array([s[:-1] for s in raw_data])
         m.train_model(model, training_data, np.array([to_expected(s) for s in raw_data]))
         error = m.test_model(model, np.array([split_data[i][:-1]]), np.array([to_expected(split_data[i])]))
-        model_weights.append((m.get_weights(model), error))
+        model_weights.append(np.array(m.get_weights(model)))
+        model_errors.append(error[0])
+
+    sum_error = sum(1/e for e in model_errors)
+
+    for idx, error in enumerate(model_errors):
+
+        proportion = (1/error)/sum_error
+        model_weights[idx] = proportion * model_weights[idx]
+        # model_weights[idx] = np.vectorize(lambda x: proportion * x)(model_weights[idx])
+
+    true_weights = sum(model_weights)
+    true_model = m.create_model(4, [8, 8, 2])
+    m.set_weights(true_model, true_weights)
+    result = m.test_model(true_model, np.array([s[:-1] for s in split_data]), np.array([to_expected(s) for s in split_data]))
+    pass
 
 
 def inv_g(x):
